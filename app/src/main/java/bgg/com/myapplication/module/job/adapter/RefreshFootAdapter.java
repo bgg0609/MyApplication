c154593cec07c8c2f4d345b98456fca34c28d3f7
huangbin <huangbin@gjk365.com>
2016-09-02 18:33:08 +0800

    jobFragment修改
    修改职位界面上拉刷新、下拉加载更多

---
+++ app/src/main/java/bgg/com/myapplication/module/job/adapter/RefreshFootAdapter.java
@@ -0,0 +1,177 @@
+package bgg.com.myapplication.module.job.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import bgg.com.myapplication.R;
+import bgg.com.myapplication.common.customview.RoundedImageView;
+import bgg.com.myapplication.module.job.model.Job;
+
+/**
+ * Created by Administrator on 2016/9/2 0002.
+ */
+public class RefreshFootAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    //上拉加载更多
+    public static final int PULLUP_LOAD_MORE = 0;
+    //正在加载中
+    public static final int LOADING_MORE = 1;
+    //上拉加载更多状态-默认为0
+    private int load_more_status = 0;
+    private LayoutInflater mInflater;
+    private List<Job> jobs = null;
+
+    private static final int TYPE_BLANK = 0;  //空数据界面
+    private static final int TYPE_ITEM = 1;  //普通Item View
+    private static final int TYPE_FOOTER = 2;  //顶部FootView
+
+
+    public RefreshFootAdapter(Context context, List<Job> mTitles) {
+        this.mInflater = LayoutInflater.from(context);
+        this.jobs = mTitles;
+    }
+
+    /**
+     * item显示类型
+     *
+     * @param parent
+     * @param viewType
+     * @return
+     */
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        //进行判断显示类型，来创建返回不同的View
+        if (viewType == TYPE_BLANK) {
+            View view = mInflater.inflate(R.layout.job_blank_item, parent, false);
+            BlankViewHolder blankViewHolder = new BlankViewHolder(view);
+            return blankViewHolder;
+        } else if (viewType == TYPE_ITEM) {
+            View view = mInflater.inflate(R.layout.job_list_item, parent, false);
+            ItemViewHolder itemViewHolder = new ItemViewHolder(view);
+            return itemViewHolder;
+        } else if (viewType == TYPE_FOOTER) {
+            View foot_view = mInflater.inflate(R.layout.recycler_load_more_layout, parent, false);
+            //这边可以做一些属性设置，甚至事件监听绑定
+            //view.setBackgroundColor(Color.RED);
+            FootViewHolder footViewHolder = new FootViewHolder(foot_view);
+            return footViewHolder;
+        }
+        return null;
+    }
+
+    /**
+     * 数据的绑定显示
+     *
+     * @param holder
+     * @param position
+     */
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        if (holder instanceof ItemViewHolder) {
+
+            Job job = jobs.get(position);
+
+            ((ItemViewHolder) holder).tvName.setText(job.getName());
+
+            ((ItemViewHolder) holder).ivAvatar.setImageResource(R.drawable.common_ic_default_avatar);
+            ((ItemViewHolder) holder).ivAvatar.setOval(false);
+            ((ItemViewHolder) holder).ivAvatar.loadImage(job.getAvatar());
+
+            holder.itemView.setTag(position);
+        } else if (holder instanceof FootViewHolder) {
+
+            FootViewHolder footViewHolder = (FootViewHolder) holder;
+
+            switch (load_more_status) {
+                case PULLUP_LOAD_MORE:
+                    footViewHolder.foot_view_item_tv.setText("上拉加载更多...");
+                    break;
+                case LOADING_MORE:
+                    footViewHolder.foot_view_item_tv.setText("正在加载更多数据...");
+                    break;
+            }
+        }
+    }
+
+    /**
+     * 进行判断是普通Item视图还是FootView视图
+     *
+     * @param position
+     * @return
+     */
+    @Override
+    public int getItemViewType(int position) {
+
+        if (jobs == null || jobs.size() == 0) {
+            return TYPE_BLANK;
+        } else if (position + 1 == getItemCount()) {
+            return TYPE_FOOTER;
+        } else {
+            return TYPE_ITEM;
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        if (jobs == null || jobs.size() == 0)
+            return 1;
+        return jobs.size() + 1;
+    }
+
+    /**
+     * //上拉加载更多
+     * PULLUP_LOAD_MORE=0;
+     * //正在加载中
+     * LOADING_MORE=1;
+     * //加载完成已经没有更多数据了
+     * NO_MORE_DATA=2;
+     *
+     * @param status
+     */
+    public void notifyDataSetChanged(int status) {
+        load_more_status = status;
+        notifyDataSetChanged();
+    }
+
+
+    /**
+     * 数据空界面
+     */
+    public static class BlankViewHolder extends RecyclerView.ViewHolder {
+//        private TextView foot_view_item_tv;
+
+        public BlankViewHolder(View view) {
+            super(view);
+//            foot_view_item_tv = (TextView) view.findViewById(R.id.tvLoadMore);
+        }
+    }
+
+    //自定义的ViewHolder，持有每个Item的的所有界面元素
+    public static class ItemViewHolder extends RecyclerView.ViewHolder {
+        public RoundedImageView ivAvatar;
+        public TextView tvName;
+
+        public ItemViewHolder(View view) {
+            super(view);
+            tvName = (TextView) view.findViewById(R.id.tvName);
+            ivAvatar = (RoundedImageView) view.findViewById(R.id.ivAvatar);
+        }
+    }
+
+    /**
+     * 底部FootView布局
+     */
+    public static class FootViewHolder extends RecyclerView.ViewHolder {
+        private TextView foot_view_item_tv;
+
+        public FootViewHolder(View view) {
+            super(view);
+            foot_view_item_tv = (TextView) view.findViewById(R.id.tvLoadMore);
+        }
+    }
+
+
+}
--- app/src/main/java/bgg/com/myapplication/module/job/fragment/JobFragment.java
+++ app/src/main/java/bgg/com/myapplication/module/job/fragment/JobFragment.java
@@ -1,7 +1,13 @@
 package bgg.com.myapplication.module.job.fragment;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.OrientationHelper;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,36 +16,111 @@ import java.util.ArrayList;
 import java.util.List;
 
 import bgg.com.myapplication.R;
-import bgg.com.myapplication.common.customview.XListView;
 import bgg.com.myapplication.common.fragment.BaseFragment;
-import bgg.com.myapplication.module.job.adapter.JobListAdapter;
+import bgg.com.myapplication.module.job.adapter.DividerItemDecoration;
+import bgg.com.myapplication.module.job.adapter.RefreshFootAdapter;
 import bgg.com.myapplication.module.job.model.Job;
 
 /**
  * Created by Administrator on 2016/9/1 0001.
  */
 public class JobFragment extends BaseFragment {
-    private XListView listview;
-    private JobListAdapter adapter;
+
+    private RecyclerView listview;
+    private RefreshFootAdapter adapter;
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private List<Job> jobs = new ArrayList<>();
+    private LinearLayoutManager linearLayoutManager;
+    private int lastVisibleItem;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View contentView = inflater.inflate(R.layout.job_fragment, null);
-        initView(contentView);
-        return contentView;
+        View view = inflater.inflate(R.layout.job_fragment, null);
+        initView(view);
+        return view;
     }
 
-    private void initView(View contentView) {
-        listview = (XListView) contentView.findViewById(R.id.lvJob);
-        List<Job> jobs = new ArrayList<>();
+    private void initView(View view) {
+
+        swipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipeRefreshLayout);
+        swipeRefreshLayout.setColorSchemeResources(R.color.swipe_color_1, R.color.swipe_color_2, R.color.swipe_color_3, R.color.swipe_color_4);
+        swipeRefreshLayout.setSize(SwipeRefreshLayout.LARGE);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                jobs.clear();
+                getJobList();
+                handler.sendEmptyMessage(1);
+            }
+        });
+
+        listview = (RecyclerView) view.findViewById(R.id.lvJob);
+        linearLayoutManager = new LinearLayoutManager(getActivity());
+        linearLayoutManager.setOrientation(OrientationHelper.VERTICAL);
+        listview.setLayoutManager(linearLayoutManager);
+        //添加分隔线
+        listview.addItemDecoration(new DividerItemDecoration(getActivity(), DividerItemDecoration.HORIZONTAL_LIST));
+        adapter = new RefreshFootAdapter(getActivity(), jobs);
+        listview.setAdapter(adapter);
+        listview.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                if (newState == RecyclerView.SCROLL_STATE_IDLE && lastVisibleItem + 1 == adapter.getItemCount()) {
+
+                    handler.sendEmptyMessage(0);
+                    getJobList();
+                    handler.sendEmptyMessage(1);
+
+                }
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                lastVisibleItem = linearLayoutManager.findLastVisibleItemPosition();
+            }
+        });
+
+        swipeRefreshLayout.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                swipeRefreshLayout.setRefreshing(true);
+                getJobList();
+                handler.sendEmptyMessage(1);
+            }
+        }, 3000);
+    }
+
+    //获取数据
+    private void getJobList() {
+        //测试数据
         for (int i = 0; i < 20; i++) {
             Job job = new Job();
             job.setAvatar("http://www.qqpk.cn/Article/UploadFiles/201409/20140910114015417_S.jpg");
             job.setName("职位" + i);
             jobs.add(job);
         }
-        adapter = new JobListAdapter(getActivity(), jobs);
-        listview.setAdapter(adapter);
     }
+
+    private MyHandler handler = new MyHandler();
+
+    class MyHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case 0:
+                    adapter.notifyDataSetChanged(RefreshFootAdapter.LOADING_MORE);
+                    break;
+                case 1:
+                    swipeRefreshLayout.setRefreshing(false);
+                    adapter.notifyDataSetChanged(RefreshFootAdapter.PULLUP_LOAD_MORE);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
 }
